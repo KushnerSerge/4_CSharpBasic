5d334fcc2a30ade54655529ce5acab2915358822
SERGEKUSHNER <kusnir.sergiu@gmail.com>
2022-02-16 01:58:37 +0200

    Add project files.

---
+++ 4_CSharpLanguageBasic/Employee.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_CSharpLanguageBasic
+{
+    public class Employee
+    {
+        protected static readonly DateTime globalStartTime;
+        public static Company Company { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+
+        // static Constructor
+        static Employee()
+        {
+            globalStartTime = DateTime.Now;
+
+            // The following statement produces the first line of output,
+            // and the line occurs only once.
+            Console.WriteLine("Static constructor sets global start time to {0}",
+                globalStartTime.ToLongTimeString());
+        }
+
+        public Employee(Company company, string name, decimal salary)
+        {
+            Company = company;
+            Name = name;
+            Salary = salary;
+        }
+        // Passed by Value
+        public decimal CalculateSalaryBonus(double bonusCoeficient)
+        {
+            return Salary * (decimal)bonusCoeficient;
+        }
+
+        // Passed by reference - going in
+        public void InfoCombine(ref decimal empSalary, ref Company e_company)
+        {
+            empSalary = Salary + empSalary;
+            e_company._rating += Company._rating;
+        }
+
+        // Passed by reference - going out
+        public  void InfoAssign(out decimal empSalary, out Company e_company)
+        {
+            empSalary = Salary;
+            e_company._rating = Company._rating * 2;
+            e_company._name = Company._name + "_NEW";
+        }
+
+        public static int GetCompanyRating()
+        {
+            return Company._rating;
+        }
+        public static string GetCompanyAddress()
+        {
+            return Company._name;
+        }
+        // Thread Example
+        public static void Thread_One_Example()
+        {
+            for(int i = 0; i <= 7; i++)
+            {
+                Console.WriteLine("Thread 1 with value: {0}", i);
+            }
+
+        }
+
+        public static void Thread_Two_Example()
+        {
+            for (int i = 0; i <= 7; i++)
+            {
+                Console.WriteLine("Thread 2 with value: {0}", i);
+            }
+
+        }
+    }
+    public struct Company
+    {
+        public string _name;
+        public int _rating;
+
+        public Company(string name, int rating)
+        {
+            this._name = name;
+            this._rating = rating;
+        }
+        
+        public static string GetCompanyInfo()
+        {
+            return "Info contact here";
+        }
+
+        public string GetCompanyName()
+        {
+            return _name;
+        }
+        public int GetCoompanyRating()
+        {
+            return _rating;
+        }
+    }
+}
---
+++ 4_CSharpLanguageBasic/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _4_CSharpLanguageBasic;
+
+Console.WriteLine("Hello, World!");
+
+// boxing and unboxing 
+int i = 1;
+object o = i + 2; // boxing  
+int j = (int)o; // unboxing 
+Console.WriteLine("i = {0}", i);
+Console.WriteLine("o = {0}", o);
+Console.WriteLine("j = {0}", j);
+
+// Create instance of value types and reference types
+Company company = new Company("One_Company", 5);  
+Employee employee = new Employee(company, "John Doe", 3220);
+
+// Using static methods
+string CompanyAddress =  Employee.GetCompanyAddress();
+int CompanyRating = Employee.GetCompanyRating();
+Console.WriteLine("Address is {0}, and rating is {1}", CompanyAddress, CompanyRating);
+
+// Write a method for each type of parameter modifier
+        // by value
+double coeficientForBonus = 1.2;
+decimal SalaryWithBonus = employee.CalculateSalaryBonus(coeficientForBonus);
+Console.WriteLine($"Salary for {employee.Name} with bonus is {SalaryWithBonus}");
+
+// Passed by reference - going in  
+decimal empSalary = employee.Salary;
+Console.WriteLine($"employeeSalary before is {empSalary}, and Company Rating before is {company._rating}");
+
+employee.InfoCombine(ref empSalary, ref company);
+
+Console.WriteLine($"employeeSalary after is {empSalary}, and Company Rating after is {company._rating}");
+
+//   Passed by reference - going out
+
+decimal newSalary;
+Company newCompany;
+employee.InfoAssign(out newSalary, out newCompany);
+Console.WriteLine($"newSalary after out is {newSalary},  Company Rating after out is {newCompany._rating}, and the new " +
+    $"Company Name is {newCompany._name}");
+
+// Static Constructor done in Employee class
+
+// Thread
+
+Thread a = new Thread(new ThreadStart(Employee.Thread_One_Example));
+Thread b = new Thread(new ThreadStart(Employee.Thread_Two_Example));
+a.Start();
+b.Start();
